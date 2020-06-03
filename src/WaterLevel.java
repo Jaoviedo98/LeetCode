617a208d6ed3728020c30a7ee48794e0cba8328a
Jorge Oveido <jaoviedo@asu.edu>
2020-06-03 16:03:53 -0700

    Second attempt with double pointer complete

--- src/WaterLevel.java
+++ src/WaterLevel.java
@@ -1,5 +1,24 @@
+/**
+ * Calculates the max water level given an array of integers, each integer is a wall
+ * Max water level is the maximum area between two walls
+ */
 class WaterLevel {
-    public int maxArea(int[] height) {
+
+    /**
+     * Used to test both methods
+     * @param args arguments for when using command line
+     */
+    public static void main (String[] args) {
+        System.out.println(maxArea(new int[] {1, 2, 3, 4, 5, 6, 8}));
+        System.out.println(maxArea2(new int[] {1, 2, 3, 4, 5, 6, 8}));
+    }
+
+    /**
+     * First attempt brute force O(n2)
+     * @param height an array of integers to calculate max area on
+     * @return the max area an Integer
+     */
+    public static int maxArea(int[] height) {
         int left = 0;
         int right = height.length - 1;
         int maxWater = 0;
@@ -14,5 +33,28 @@ class WaterLevel {
             }
         }
         return maxWater;
+    }
+
+    /**
+     * Second attempt, double pointer O(n)
+     * @param height an array of integers to calculate max area on
+     * @return the max area, an Integer
+     */
+    public static int maxArea2 (int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int maxLevel = 0;
+        while (left < right) {
+            int min = Integer.min(height[left], height[right]);
+            int difference = right - left;
+            maxLevel = Integer.max(maxLevel, difference * min);
+            if (height[left] < height[right]) {
+                left++;
+            }
+            else {
+                right--;
+            }
+        }
+        return maxLevel;
     }
 }
